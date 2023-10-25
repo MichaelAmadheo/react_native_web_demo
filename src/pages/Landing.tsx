cd1722e97d1dda5b3c71a56769b49d97d4a84812
Michael Amadheo <michael.amadheo@sera.astra.co.id>
2023-10-25 16:43:04 +0700

    Fixed wrong component name call

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,6 +1,6 @@
 import {View} from 'react-native';
 import React from 'react';
-import Header from '../components/header';
+import Header from '../components/Header';
 
 const Landing = () => {
   return (
