362a493e18b08a4a5aa23865569e92013306d9ba
Michael Amadheo <michael.amadheo@sera.astra.co.id>
2023-10-25 16:35:06 +0700

    Changed App content to load Landing

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView} from 'react-native';
 import React from 'react';
+import Landing from './src/pages/Landing';
 
 const App = () => {
   return (
     <SafeAreaView>
-      <Text>App</Text>
+      <Landing />
     </SafeAreaView>
   );
 };
